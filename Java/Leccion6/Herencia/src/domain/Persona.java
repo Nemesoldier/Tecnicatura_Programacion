faecc09da13a94e47fb61d9b3055e56d7f69a6ae
Matias Gomez <nemesoldier@gmail.com>
2024-10-10 21:33:15 -0300

    Subo clases actualizada al repositorio propio fecha 10/10

---
+++ Java/Leccion6/Herencia/src/domain/Persona.java
@@ -0,0 +1,77 @@
+
+package domain;
+
+
+public class Persona {
+    //Atributos de herencia
+    protected String nombre;
+    protected char genero;
+    protected int edad;
+    protected String direccion;
+    
+    
+    //Constructor vacio: este es para crear objeros sin necesidad de inicializar
+    //Los atributos de la clase
+    public Persona(){  //Constructor 1
+        
+        
+    }
+    
+    public Persona(String nombre){ //Constructor 2
+        this.nombre = nombre;
+    }
+
+    public Persona(String nombre, char generno, int edad, String direccion) {  //Constructor 3
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+         this.nombre = nombre;
+    }
+
+    public char getGenerno() {
+        return this.genero;
+    }
+
+    public void setGenerno(char generno) {
+        this.genero = generno;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona{");
+        sb.append("nombre=").append(nombre);
+        sb.append(", genero=").append(genero);
+        sb.append(", edad=").append(edad);
+        sb.append(", direccion=").append(direccion);
+        
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
---
+++ Java/Leccion6/Herencia/src/test/TestHerencia.java
@@ -0,0 +1,17 @@
+
+package test;
+
+import domain.Cliente;
+import domain.Empleado;
+import java.util.Date;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Ariel", 57000.00);
+        System.out.println("empleado1 = " + empleado1);
+        
+        Cliente cliente1 = new Cliente(new Date(), true, "Bety", 'F', 32, "9 de julio 1413");
+        System.out.println("cliente = " + cliente1);
+    }
+    
+}
