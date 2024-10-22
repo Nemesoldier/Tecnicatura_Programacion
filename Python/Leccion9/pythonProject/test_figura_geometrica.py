b56e60d298667bfc272a4a67ae2edcf8cf3acf02
Nemesoldier <tecnica@burmansas.com>
2024-10-22 15:09:10 -0300

    subo al repositorio, continuar desde el video 3

--- Python/Leccion9/pythonProject/test_figura_geometrica.py
+++ Python/Leccion9/pythonProject/test_figura_geometrica.py
@@ -1,7 +1,10 @@
 from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
 
-cuadrado1 = Cuadrado(5, "Azul")
+
+print("Creacion de objeto clase Cuadrado".center(50, "_"))
+cuadrado1 = Cuadrado(8, "Azul")
+cuadrado1.alto = -10
 print(cuadrado1.ancho)
 print(cuadrado1.alto)
 print(f"Calculo del area del cuadrado: {cuadrado1.calcular_area()}")
@@ -10,7 +13,8 @@ print(f"Calculo del area del cuadrado: {cuadrado1.calcular_area()}")
 print(Cuadrado.mro())
 
 print(cuadrado1)
-
-rectangulo1 = Rectangulo(3, 8, "Verde")
+print("Creacion de objeto clase Rectangulo".center(50,"_"))
+rectangulo1 = Rectangulo(3, 9, "Verde")
+rectangulo1.ancho = 15
 print(f"Claculo del area del rectangulo: {rectangulo1.calcular_area()}")
 print(rectangulo1)
